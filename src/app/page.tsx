209d78b2847c3499508df4423cecdb73aa0acb6b
OMI786I <sohrawardy1998@gmail.com>
2024-11-25 01:56:05 +0600

    added cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Wavy, WavyBackgroundDemo } from "@/components/Wavy";
 export default function Home() {
   return (
     <>
-      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
         <HeroSection />
         <FeaturedSection />
         <ChooseUs />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-black rounded-lg shadow m-4 ">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-black  rounded-lg shadow m-4 ">
+      <div className="w-full h-screen mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://flowbite.com/" className="hover:underline">
